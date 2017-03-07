fb46a91db18bd1a41d4e112dda9817bd334e4402
Hoku <hoku.oh@gmail.com>
2017-03-07 05:31:38 -0800

    hero text

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,53 +1,20 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router';
-import { CounterButton, GithubButton } from 'components';
-import config from '../../config';
+import { CounterButton } from 'components';
 import Helmet from 'react-helmet';
 
 export default class Home extends Component {
   render() {
     const styles = require('./Home.scss');
     // require the logo image both from client and server
-    const logoImage = require('./logo.png');
     return (
       <div className={styles.home}>
         <Helmet title="Home"/>
-        <div className={styles.masthead}>
-          <div className="container">
-            <div className={styles.logo}>
-              <p>
-                <img src={logoImage}/>
-              </p>
-            </div>
-            <h1>{config.app.title}</h1>
+        <div className="container text-center">
+          <h2>Next Generation Messaging</h2>
 
-            <h2>{config.app.description}</h2>
-
-            <p>
-              <a className={styles.github} href="https://github.com/erikras/react-redux-universal-hot-example"
-                 target="_blank">
-                <i className="fa fa-github"/> View on Github
-              </a>
-            </p>
-            <GithubButton user="erikras"
-                          repo="react-redux-universal-hot-example"
-                          type="star"
-                          width={160}
-                          height={30}
-                          count large/>
-            <GithubButton user="erikras"
-                          repo="react-redux-universal-hot-example"
-                          type="fork"
-                          width={160}
-                          height={30}
-                          count large/>
-
-            <p className={styles.humility}>
-              Created and maintained by <a href="https://twitter.com/erikras" target="_blank">@erikras</a>.
-            </p>
-          </div>
+          <p className="lead">Square One Labs is building next generation messaging products. Be the first to<br/> try them before they launch. We'll text you.</p>
         </div>
-
         <div className="container">
           <div className={styles.counterContainer}>
             <CounterButton multireducerKey="counter1"/>
