8eb5a8ec3d1da273f53b884dbfcc597918c78a51
Hoku <hoku.oh@gmail.com>
2017-03-07 17:58:31 -0800

    before adding text input

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -12,6 +12,7 @@ export default class Home extends Component {
           <div className="text-center">
             <h2>Next Generation Messaging</h2>
             <p className="lead">Square One Labs is building next generation messaging products. Be the first to<br/> try them before they launch. We'll text you.</p>
+            <p>Enter your number to begin</p>
           </div>
         </div>
       </div>
