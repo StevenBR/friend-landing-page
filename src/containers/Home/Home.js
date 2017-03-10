b7b6543439d0798be67c510f41e7f7d5ef74b7f7
Hoku <hoku.oh@gmail.com>
2017-03-09 21:17:15 -0800

    finish hero

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
+import { Hero } from 'components';
 
 export default class Home extends Component {
   render() {
-    const styles = require('./Home.scss');
     // require the logo image both from client and server
     return (
-      <div className={styles.home}>
+      <div>
         <Helmet title="Home"/>
-        <div className="container">
-          <div className="text-center">
-            <h2>Next Generation Messaging</h2>
-            <p className="lead">Square One Labs is building next generation messaging products. Be the first to<br/> try them before they launch. We'll text you.</p>
-            <p>Enter your number to begin</p>
-          </div>
-        </div>
+        <Hero />    
       </div>
     );
   }
