95b0b4f93a501c4c6cbcfa3f649afc10c912a21d
Hoku <hoku.oh@gmail.com>
2017-04-12 20:13:15 -0700

    console log on click

--- src/components/NumberCaptureForm/NumberCaptureForm.js
+++ src/components/NumberCaptureForm/NumberCaptureForm.js
@@ -3,17 +3,23 @@ import MaskedInput from 'react-text-mask';
 
 
 export default class NumberCaptureForm extends Component {
+	submitNumber = (e) => {
+		e.preventDefault();
+		console.log('clicked button', this.refs.number);
+	}
+
 	render () {
 		const styles = require("./NumberCaptureForm.scss");
 		return (
 			<div className={styles.numberCaptureForm}>
-				<form action="" className="form-inline">
+				<form action="" className="form-inline" onSubmit={this.submitNumber}>
 					<div className="form-group">
 						<MaskedInput
 						mask={['(',/[1-9]/,/\d/,/\d/,')',' ',/\d/,/\d/,/\d/,'-',/\d/,/\d/,/\d/,/\d/]} 
 						placeholder="(555) 555-5555"
 						className="form-control"
 						guide={false}
+						ref="number"
 						/>
 					</div>
 					<button className={`btn btn-${this.props.buttonType}`}>TEXT ME</button>					
