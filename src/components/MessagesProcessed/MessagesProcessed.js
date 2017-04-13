6d7edf73d7ac67be7faa3ce0a8961f1e30b3115b
Hoku <hoku.oh@gmail.com>
2017-04-12 19:44:27 -0700

    remove console log

--- src/components/MessagesProcessed/MessagesProcessed.js
+++ src/components/MessagesProcessed/MessagesProcessed.js
@@ -10,9 +10,6 @@ export default class MessagesProcessed extends Component {
 
 	componentWillMount () {
 		this.props.getMessages(Messages.post());
-		console.log(this.props.messagesPromise);
-		console.log('Component WILL MOUNT!');		
-
 	}
 
 	returnMessageCount = () => {
@@ -20,7 +17,7 @@ export default class MessagesProcessed extends Component {
 			let messageCount = this.props.messagesPromise.result * 2;
 			return (messageCount.toLocaleString());
 		}
-		return ('loading');
+		return ('counting...');
 	}
 
 	render () {
