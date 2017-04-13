87fa512b6407f0f0ccceb89e470dcdd6fa242b03
Hoku <hoku.oh@gmail.com>
2017-04-13 05:32:38 -0700

    delete excess

--- src/redux/modules/submitNumber.js
+++ src/redux/modules/submitNumber.js
@@ -1,29 +1,3 @@
-// const INITIAL_SUBMIT = 'INITIAL_SUBMIT';
-// const SUBMISSION_SUCCESSFUL = 'SUBMISSION_SUCCESSFUL';
-// const SUBMISSION_FAILED = 'SUBMISSION_FAILED';
-
-// export default function reducer (state={}, action) {
-// 	switch (action.payload) {
-// 		case INITIAL_SUBMIT:			
-// 			return {
-// 				...state,
-// 				loading: true,
-// 				numberPromise: action.payload
-// 			}
-// 		default:
-// 			return state
-// 	}
-// }
-
-// export function submitNumber (payload) {
-// 	console.log('first',payload);
-// 	return {
-// 		type: INITIAL_SUBMIT,
-// 		payload
-// 	}
-// }
-
-
 const LOAD = 'submit-number/LOAD';
 const LOAD_SUCCESS = 'submit-number/LOAD_SUCCESS';
 const LOAD_FAIL = 'submit-number/LOAD_FAIL';
